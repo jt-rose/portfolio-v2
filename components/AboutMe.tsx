4876421d1ba2abe1c8b9a7b211717936b3d122cb
Jeffrey Rose <jtr219@outlook.com>
2022-02-02 11:27:38 -0500

    add about me section

---
+++ components/AboutMe.tsx
@@ -0,0 +1,36 @@
+export const AboutMe = () => {
+  return (
+    <section id="aboutme">
+      <h1>About Me</h1>
+      <p>
+        <img
+          id="me-and-the-boys"
+          src="./assets/personal/me_and_the_boys.jpg"
+          alt=""
+        />
+        For most of the last decade I've worked as a project manager at a market
+        research firm, where uncovering and analyzing large collections of data
+        was an essential part of my role. This gradually led me to an interest
+        in automation and business intelligence, both of which started me down
+        the path of programming.
+        <br />
+        <br />
+        What started off as building simple terminal games in Python eventually
+        grew into an interest in web development, where I started to experiment
+        with building some (at the time, very ugly) websites and first wrapping
+        my head around JavaScript and JQuery. Since then, I've gone on to learn
+        React, TypeScript, Node, SQL, Docker, GraphQL, and recently Golang.
+        <br />
+        <br />
+        I enjoy exploring the React ecosystem as well as improving my backend
+        skills by building new GraphQL servers. I'm excited by all the
+        advancements that have swept through web development over the last few
+        years and hope I can contribute to a small part of it.
+        <br />
+        <br />
+        When I'm not fighting the computer, I enjoy spending time with my young
+        boys, gaming, and reading Japanese novels.
+      </p>
+    </section>
+  );
+};
