fdbd645dd9a144d16746ee6019f255a76839447f
Jeffrey Rose <jtr219@outlook.com>
2022-02-02 11:36:54 -0500

    set up unformatted index structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,33 @@
-import Gallery from '../components/Gallery'
+import { Head } from "../components/Head";
+import { Header } from "../components/Header";
+import { Intro } from "../components/Intro";
+import { Skills } from "../components/Skills";
+import { AboutMe } from "../components/AboutMe";
+import { ProjectsModal, ProjectsListing } from "../components/Projects";
+import { Experience } from "../components/Experience";
+import { Contact } from "../components/Contact";
+//import Gallery from "../components/Gallery";
 
-const Index = () => <Gallery />
+//const Index = () => <Gallery />;
 
-export default Index
+// main
+// components
+// /main
+
+const Index = () => {
+  return (
+    <main>
+      <Head />
+      <Header />
+      <Intro />
+      <Skills />
+      <AboutMe />
+      <ProjectsModal />
+      <ProjectsListing />
+      <Experience />
+      <Contact />
+    </main>
+  );
+};
+
+export default Index;
