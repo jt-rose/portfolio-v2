e843e0385aa5795f37437ed0a0a6b4b94b483a02
Jeffrey Rose <jtr219@outlook.com>
2022-02-02 11:27:18 -0500

    add intro section

---
+++ components/Intro.tsx
@@ -0,0 +1,21 @@
+export const Intro = () => {
+  return (
+    <section id="intro">
+      <img
+        id="intro-pic"
+        src="./assets/personal/self.jpeg"
+        alt="Self Portrait"
+      />
+      <h1>Jeff Rose</h1>
+      <h3>Full Stack Web Developer</h3>
+      <p>
+        Hello and welcome to my site! I am a fullstack developer focusing on
+        TypeScript, React, NextJS, and GraphQL. I'm a functional programming
+        enthusiast and have recently begun exploring Golang and Rust as well.
+        When I'm not busy with my boys, I can usually be found digging into a
+        new library or building out another side project. If you'd like to work
+        together please reach out! Happy Coding!
+      </p>
+    </section>
+  );
+};
