4fd822996fc0bbb65ab9f712322870a7ac384d66
Jeffrey Rose <jtr219@outlook.com>
2022-02-02 11:27:29 -0500

    add skills section

---
+++ components/Skills.tsx
@@ -0,0 +1,51 @@
+export const Skills = () => {
+  return (
+    <section id="skills">
+      <h1>Skills</h1>
+      <ul id="skill-logos">
+        <li>
+          <img
+            src="./assets/skill-logos/coding-web-development-svgrepo-com.svg"
+            alt=""
+          />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/typescript.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/nodejs-1.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/python-5.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/golang-1.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/react-2.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/redux.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/graphql.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/postgresql.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/redis.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/git.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/mocha-1.svg" alt="" />
+        </li>
+        <li>
+          <img src="./assets/skill-logos/docker.svg" alt="" />
+        </li>
+      </ul>
+    </section>
+  );
+};
